6ada149b3040c3d414e93934cb11d2b848b4b4ed
Serg ChepS <sachep64@gmail.com>
2022-08-29 14:06:59 +0300

    280822

--- java/idea/idpro/LC1_Two_Sum/src/LC1_Two_Sum.java
+++ java/idea/idpro/LC1_Two_Sum/src/LC1_Two_Sum.java
@@ -1,9 +1,10 @@
 class Sol{
-    public int[]  solution(int nums[],int target){
+    public int[]  solution(int[] nums,int target){
         //int[] a;
         //a =new int[2];//массив для занесения туда индексов - ответов
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
+                LC1_Two_Sum.kol_op++;
                 if(nums[i]+nums[j]==target){
                    // a[0]=i;
                    // a[1]=j;
@@ -19,17 +20,31 @@ class Sol{
 
 
 public class LC1_Two_Sum {
+    static int kol_op;
     public static void main(String[] args) {
-        int[] nums={2,7,11,15};
         int target;
-        target=9;
+        target=9000;
+        int[] nu={2,7,11,15};
+        int[] nums =new int[100000];// большой массив для экспериментов
+        for(int i=0;i<nums.length;i++){
+            nums[i]=(int) (Math.random()*100000);
+        }
+        //nums=nu;
         int[] b;//для решения
         Sol s = new Sol();
+        kol_op=0;
         b=s.solution(nums,target);
-        for (int a:nums){
-            System.out.print(a+" ");
+        System.out.print(kol_op+" количество сравнений ");
+        //for (int a:nums){
+          //  System.out.print(a+" ");
+        //}
+        if (b!=null) {
+            System.out.println();
+            System.out.println(b[0] + " " + b[1] + " " + nums[b[0]] + "+" + nums[b[1]] + "=" + target);
         }
-        System.out.println();
-        System.out.println(b[0]+" "+b[1]+" "+nums[b[0]]+"+"+nums[b[1]]+"="+target);
+        else {
+            System.out.println("решения нет !");
+
+        }
     }
 }
