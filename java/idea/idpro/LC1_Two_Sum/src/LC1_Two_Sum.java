f8d7f84503d11b99ddad78d91738ba4eaf964259
Serg ChepV <sachep64@gmail.com>
2022-08-29 16:20:45 +0300

    290822

--- java/idea/idpro/LC1_Two_Sum/src/LC1_Two_Sum.java
+++ java/idea/idpro/LC1_Two_Sum/src/LC1_Two_Sum.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// первый подход -прямой перебор
 class Sol{
     public int[]  solution(int[] nums,int target){
         //int[] a;
@@ -17,23 +21,50 @@ class Sol{
     }
 
 }
-
+//второй подход с использование хэш таблицы
+class Sol2HashT{
+    public int[] solution(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
 
 public class LC1_Two_Sum {
-    static int kol_op;
+    static int kol_op;//для подсчета количества операций
+
+    //для подсчета количества оперативной памяти занимаемым в какой-то момент
+    public static long mem() {
+        Runtime runtime = Runtime.getRuntime();
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
     public static void main(String[] args) {
         int target;
-        target=9000;
-        int[] nu={2,7,11,15};
-        int[] nums =new int[100000];// большой массив для экспериментов
+        target=900000;
+        int[] nu={2,7,11,15};//маленький массив для начала
+        int[] nums =new int[100000000];// большой массив для экспериментов
         for(int i=0;i<nums.length;i++){
             nums[i]=(int) (Math.random()*100000);
         }
         //nums=nu;
         int[] b;//для решения
-        Sol s = new Sol();
+        //Sol s = new Sol();//первый подход
+        Sol2HashT s=new Sol2HashT(); //второе решение
         kol_op=0;
+        long time = System.currentTimeMillis();//начало выполнения
+        long befo=mem();// начало память
         b=s.solution(nums,target);
+        System.out.println(System.currentTimeMillis()-time+" мс выполнялось");// конец выполнения
+        System.out.println(mem()-befo+" памяти заняло");// конец выполнения память
         System.out.print(kol_op+" количество сравнений ");
         //for (int a:nums){
           //  System.out.print(a+" ");
