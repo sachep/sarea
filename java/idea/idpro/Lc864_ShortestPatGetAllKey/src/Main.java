b468f74528ce1954b2eba4c1b0605b7a923b449f
Serg ChepV <sachep64@gmail.com>
2022-09-28 12:23:48 +0300

    280922 1223

--- java/idea/idpro/Lc864_ShortestPatGetAllKey/src/Main.java
+++ java/idea/idpro/Lc864_ShortestPatGetAllKey/src/Main.java
@@ -36,7 +36,6 @@ class Solution{
         int cols = grid[0].length();
         int[][] dirs = { {1,0} , {0,1} , {0,-1} , {-1,0} };
         Set<String> visited = new HashSet<>();
-
         int steps=0;
         Queue<Node> queue = new LinkedList<>();
         int keyCount=0;
@@ -44,7 +43,7 @@ class Solution{
             for(int j=0; j < cols; j++) {
                 if(grid[i].charAt(j) == '@') {
                     queue.add(new Node(i , j , ""));
-                    System.out.println("Node @");
+                    System.out.println("Начало установлено Node @");
                 }
                 else if(grid[i].charAt(j)>= 'a' && grid[i].charAt(j) <= 'f') {
                     keyCount++;
@@ -54,18 +53,19 @@ class Solution{
 
         while(!queue.isEmpty()) {
             int size = queue.size();
+            System.out.println("размер очереди на данный момент "+size);
             for(int i=0; i<size; i++) {
                 Node temp = queue.poll();
                 int r = temp.row;
                 int c = temp.col;
                 String key = temp.key;
-                System.out.println(r+":"+c+":"+key+":");
+                System.out.println("Взяли из очереди "+r+":"+c+":"+key+":");
                 if(!visited.contains(temp.toString(r , c , key))) {
                     visited.add(temp.toString(r, c, key));
-                    System.out.println("visited.add "+r+":"+c+":"+key+":");
+                    System.out.println("занесли в visited.add "+r+":"+c+":"+key+":");
                 }
                 else {
-                    System.out.println("Пропущено");
+                    System.out.println("был ранее посещен Пропущено");
                     continue;
                 }
                 if(key.length() == keyCount)
@@ -74,7 +74,7 @@ class Solution{
                 for(int[] dir : dirs) {
                     int nr = r+dir[0];
                     int nc = c+dir[1];
-                    System.out.println("dir "+nr+":"+nc);
+                    System.out.println("Цикл по направлениям dir "+nr+":"+nc);
                     Node newNode = new Node(nr , nc , key);
                     if(nr<0 || nr>=rows || nc<0 || nc >=cols || grid[nr].charAt(nc) == '#' || visited.contains(newNode.toString(nr , nc , key))) {
                         continue;
@@ -83,15 +83,16 @@ class Solution{
                     char ch = grid[nr].charAt(nc);
                     if(key.indexOf(ch) == -1 && ch >= 'a' &&  ch <= 'f') {
                         queue.add(new Node(nr , nc , key+ch));
-                        System.out.println("Node "+nc+":"+nr+":"+key+":"+ch);
+                        System.out.println("добавили в очередь с ключем Node "+nc+":"+nr+":"+key+":"+ch);
 
                     }
                     else if( ch>='A' && ch <= 'F' && key.indexOf(Character.toLowerCase(ch)) == -1)  {
+                        System.out.println(" нашли звмок а ключа нет - пропущено");
                         continue;
                     }
                     else {
                         queue.add(new Node(nr , nc , key));
-                        System.out.println("Node "+nc+":"+nr+":"+key);
+                        System.out.println("добавили в очередь без ключа Node "+nc+":"+nr+":"+key);
 
                     }
                 }
@@ -104,8 +105,8 @@ class Solution{
 
 public class Main {
     public static void main(String[] args) {
-        //        String[] grid= {"@.a..","###.#","b.A.B"}; // оригинальное усл задачи ответ 8
-        String[] grid= {"@#a..",".....","b#A.B"}; //
+                String[] grid= {"@.a..","###.#","b.A.B"}; // оригинальное усл задачи ответ 8
+        //String[] grid= {"@#a..",".....","b#A.B"}; //
         Solution s=new Solution();
         System.out.println("Ходов: "+s.shortestPathAllKeys(grid));
     }
